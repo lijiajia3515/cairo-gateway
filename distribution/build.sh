1264f18bbd268b380749eafa63c55ad3727e68ce
lijiajia <lijiajia3515@163.com>
2021-01-04 14:43:17 +0800

    docker build

---
+++ distribution/build.sh
@@ -0,0 +1,2 @@
+docker build -t registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-gateway:snapshot -f Dockerfile .
+docker push registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-gateway:snapshot
--- src/main/java/com/hfhk/gateway/GatewayApp.java
+++ src/main/java/com/hfhk/gateway/GatewayApp.java
@@ -1,4 +1,4 @@
-package com.hfhk.harpoon.gateway;
+package com.hfhk.gateway;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
